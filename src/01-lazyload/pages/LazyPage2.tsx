33cd289d359eff8331f49561af1dae1cee929fa4
jfranciscogr <j_francisco_gr@hotmail.com>
2023-01-23 13:05:50 -0600

    lazyload basico

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const LazyPage2 = () => {
+	return <div>LazyPage2</div>;
+};
+export default LazyPage2;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Navigation } from './01-lazyload/router/Navigation';
+
+import './styles.css';
+
+function App() {
+	return (
+		<>
+			<Navigation />
+		</>
+	);
+}
+
+export default App;
